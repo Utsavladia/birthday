64ba434d2ed37c076a007badaf9a447a3b709e62
Utsavladia <utsavladia@gmail.com>
2024-04-02 19:56:52 +0530

    video

--- src/h2.js
+++ src/h2.js
@@ -43,6 +43,7 @@ const cards = [
 
 const H2 = () => {
   const [gift, setgift] = useState(false);
+  const [celeb, setCeleb] = useState(false);
   const handlegift = () => {
     setgift((prev) => !prev);
   };
@@ -60,7 +61,8 @@ const H2 = () => {
     }
   };
 
-  const handlecelebrate = () => {
+  const handlecelebrate = async () => {
+    await setCeleb(true);
     const s4 = document.getElementById("s4");
     if (s4) {
       s4.scrollIntoView({ behavior: "smooth" });
@@ -212,17 +214,19 @@ const H2 = () => {
           </button>
         </div>
       </section>
-      <section id="s4">
-        <div className="min-h-screen relative w-screen">
-          <video
-            src={bgvdo}
-            className="absolute inset-0 w-full h-full object-cover"
-            autoPlay
-            loop
-            playsInline
-          ></video>
-        </div>
-      </section>
+      {celeb && (
+        <section id="s4">
+          <div className="min-h-screen relative w-screen">
+            <video
+              src={bgvdo}
+              className="absolute inset-0 w-full h-full object-cover"
+              autoPlay
+              loop
+              playsInline
+            ></video>
+          </div>
+        </section>
+      )}
     </div>
   );
 };
